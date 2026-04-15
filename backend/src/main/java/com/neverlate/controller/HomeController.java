09d83f4aac61cdf394c69dcef48904ce6eadec12
David <idurbo@gmail.com>
2026-04-15 14:17:09 -0400

    Add HomeController with index mapping

---
+++ backend/src/main/java/com/neverlate/controller/HomeController.java
@@ -0,0 +1,7 @@
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String index() {
+        return "index"; // This must match a file in src/main/resources/templates/index.html
+    }
+}
